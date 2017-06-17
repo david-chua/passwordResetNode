7c037edfd127d5360e372a2285e412b8e2fb0bcb
david-chua <dp_chua@hotmail.com>
2017-06-17 03:13:49 -0400

    added passport for password

--- app.js
+++ app.js
@@ -6,11 +6,80 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
-var nodemailer = require('passport');
+var nodemailer = require('nodemailer');
+var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
 var async = require('async');
 var crypto = require('crypto');
+
+// Passport for password
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function (err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false, { message: 'Incorrect username. ' });
+    user.comparePassword(password, function(err, isMatch) {
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// User model
+/*
+ Note: Properties resetPasswordToken and resetPassword won't be part of the created model, because they are set only after password reset is submitted so those properties would not be set when creating a new user.
+*/
+var userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSALT(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null,function(err, has) {
+      if (err) return next (err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+//interact with database
+mongoose.connect('localhost');
 
 var app = express();
 
@@ -24,6 +93,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(session({ secret: 'session secret key' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
@@ -34,17 +105,4 @@ app.get('/', function(req,res) {
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
-});
-
-
-// User model
-/*
- Note: Properties resetPasswordToken and resetPassword won't be part of the created model, because they are set only after password reset is submitted so those properties would not be set when creating a new user. 
-*/
-var userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
 });
